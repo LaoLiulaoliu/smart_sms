075de9ca98a5c824ea70b8e9142f7ebcab02b44c
dan <rq23456@yeah.net>
2014-11-26 18:48:01 +0800

    change call method with rails deliver

--- lib/smart_sms/has_sms_verification.rb
+++ lib/smart_sms/has_sms_verification.rb
@@ -114,11 +114,11 @@ module SmartSMS
 
         # 发送短信至手机
         #
-        def deliver(text = SmartSMS::VerificationCode.random,
-                    options = {})
+        def deliver(text: SmartSMS::VerificationCode.random,
+                    tpl_id: SmartSMS.config.template_id)
           result = SmartSMS.deliver send(self.class.sms_mobile_column),
                                     text,
-                                    tpl_id: options[:template_id] || SmartSMS.config.template_id
+                                    tpl_id: tpl_id
           if result['code'] == 0
             sms = SmartSMS.find_by_sid(result['result']['sid'])['sms']
             save_or_return_message sms, text
